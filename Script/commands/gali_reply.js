37511f52d97f0ba9ab859610e6adf58cae0427eb
imran171811 <imrsnhossain319@gmail.com>
2026-01-06 18:18:30 +0600

    Update gali_reply.js

--- Script/commands/gali_reply.js
+++ Script/commands/gali_reply.js
@@ -5,7 +5,7 @@ module.exports.config = {
 	hasPermssion: 0,
 	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️", 
 	description: "+",
-	commandCategory: "+",
+	commandCategory: "gali",
 	usages: "abal",
     cooldowns: 3, 
 };
