6824e2f03c73799da838f4920134ff26d4fc1da5
imran171811 <imrsnhossain319@gmail.com>
2026-01-06 18:15:50 +0600

    Update gali_reply.js

--- Script/commands/gali_reply.js
+++ Script/commands/gali_reply.js
@@ -4,17 +4,17 @@ module.exports.config = {
     version: "1.0.1",
 	hasPermssion: 0,
 	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️", 
-	description: "no prefix",
-	commandCategory: "no prefix",
+	description: "+",
+	commandCategory: "+",
 	usages: "abal",
-    cooldowns: 5, 
+    cooldowns: 3, 
 };
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	if (event.body.indexOf("fuck")==0 || event.body.indexOf("mc")==0 || event.body.indexOf("chod")==0 || event.body.indexOf("bal")==0 || event.body.indexOf("bc")==0 || event.body.indexOf("maa ki chut")==0 || event.body.indexOf("xod")==0 || event.body.indexOf("behen chod")==0 || event.body.indexOf("🖕")==0 || event.body.indexOf("madarchod")==0 || event.body.indexOf("chudi")==0 || event.body.indexOf("gala gali")==0) {
 		var msg = {
-				body: "(Boss Dk , Gali q Dete Ho. Lund Katke Hath M rakh Dunga)",
+				body: "(tor mayre bap,Tor 14 gushti mari,Shalar baccha)",
 			}
 			api.sendMessage(msg, threadID, messageID);
 		}
